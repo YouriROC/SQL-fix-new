e79e7ca0433164d58bfc55a1e2f34e303291448e
Youri <55980372+YouriROC@users.noreply.github.com>
2020-03-05 22:43:55 +0100

    v 0.1

---
+++ SQL Fixer - OND/SQL Fixer - OND/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace SQL_Fixer___OND
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.downloadFixedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.devmodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.downloadFixedToolStripMenuItem,
+            this.devmodeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // downloadFixedToolStripMenuItem
+            // 
+            this.downloadFixedToolStripMenuItem.Name = "downloadFixedToolStripMenuItem";
+            this.downloadFixedToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.downloadFixedToolStripMenuItem.Text = "Download fixed";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(273, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(502, 411);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(59, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Fix!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // devmodeToolStripMenuItem
+            // 
+            this.devmodeToolStripMenuItem.Name = "devmodeToolStripMenuItem";
+            this.devmodeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.devmodeToolStripMenuItem.Text = "Dev-Logs";
+            this.devmodeToolStripMenuItem.Click += new System.EventHandler(this.devmodeToolStripMenuItem_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.DimGray;
+            this.textBox2.ForeColor = System.Drawing.Color.LightGreen;
+            this.textBox2.Location = new System.Drawing.Point(12, 304);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(240, 134);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SQL Fix - Youri Schuilenburg";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem downloadFixedToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem devmodeToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ SQL Fixer - OND/SQL Fixer - OND/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_Fixer___OND
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String unname = "";
+        bool DevActive = false;
+        String DevLog = "The app started.\n";
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            unname = "";
+            OpenFileDialog unfixed = new OpenFileDialog();
+            unfixed.Filter = "APP ONLY ALLOWS .txt|*.txt";
+
+            if (unfixed.ShowDialog().Equals(DialogResult.OK))
+            {
+                unname = unfixed.FileName;
+                DevLog = DevLog + "User added file: " + unname;
+                refreshDevLog();
+                textBox1.Text = File.ReadAllText(unname, Encoding.UTF8);
+                button1.Enabled = true;
+            }
+            else
+            {
+                DevLog = DevLog + "User failed to find a file.";
+                refreshDevLog();
+                MessageBox.Show("Failed to find a file.");
+                button1.Enabled = false;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DevLog = DevLog + "User started fixing the file...";
+            String AttemptFix = File.ReadAllText(unname, Encoding.UTF8);
+            refreshDevLog();
+            string CleanGo = getBetween(AttemptFix, "g", "o");
+            textBox1.Text = CleanGo;
+            DevLog = DevLog + "Did it fix?";
+        }
+
+        public static string getBetween(string AttemptFix, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (AttemptFix.Contains(strStart) && AttemptFix.Contains(strEnd))
+            {
+                Start = AttemptFix.IndexOf(strStart, 0) + strStart.Length;
+                End = AttemptFix.IndexOf(strEnd, Start);
+                return AttemptFix.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+
+        }
+
+        private void devmodeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (DevActive == true)
+            {
+                DevActive = false;
+                textBox2.Visible = false;
+            }
+            else
+            {
+                DevActive = true;
+                textBox2.Visible = true;
+            }
+        }
+
+        public void refreshDevLog()
+        {
+            textBox2.Text = DevLog;
+        }
+    }
+}
